ddef1225708f0ca816408b0ddbd83ea0f9d11736
u16suzu <u16suzu@gmail.com>
2014-08-28 14:49:36 +0900

    1st

---
+++ Singleton/Objective-C Singleton class.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,37 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAMEASIDENTIFIER___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+static ___FILEBASENAMEASIDENTIFIER___ *sharedData_ = nil;
+
+#pragma mark - Public Method
+
++ (id)sharedInstance {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedData_ = [___FILEBASENAMEASIDENTIFIER___ new];
+    });
+    return sharedData_;
+}
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        //Initialization
+    }
+    return self;
+}
+
+
+@end
